7e8b0159c699c241d2bea607cfa2e63983c19664
Jonas Kerber <kerber.jonas@googlemail.com>
2019-01-25 12:39:11 +0100

    docker_run.sh: Fixed bug
    folder work now mounts correctly

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name pytorch_notebook -p 8888:8888 -v $(pwd)/exchange:/home/jovyan/exchange -v $(pwd)/notebooks:/home/jovyan/work pytorch_notebook_img
+docker run -it --rm --name pytorch_notebook -p 8888:8888 -v $(pwd)/exchange:/home/jovyan/exchange -v $(pwd)/work:/home/jovyan/work pytorch_notebook_img
